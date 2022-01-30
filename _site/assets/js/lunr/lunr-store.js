8334667c14aa98cfbe8c64802f67d13c503a1cb6
Namsub KIm <sub64886488@gmail.com>
2022-01-31 00:52:23 +0900

    third post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,31 @@
-var store = []
+var store = [{
+        "title": "Unity programming(1)",
+        "excerpt":"Unity 시작하기  본 글은 “레트로의 유니티 게임 프로그래밍 에센스”를 따라가며 작성될 예정   시작해보자!      ","categories": [],
+        "tags": [],
+        "url": "/Unity-Programming(1)/",
+        "teaser": null
+      },{
+        "title": "Unity programming(2)",
+        "excerpt":"1. 유니티 인터페이스 기본적인 유니티 인터페이스 설정을 해보고 살펴보자 초기화면 셋팅 유니티 프로젝트를 열면 다음과 같은 모습이다. 초기화면 이제 우측 상단의 ‘Default’라고 되어 있는 부분을 클릭해서 ‘2 by 3’로 바꿔주고 ‘Project’ 탭을 ‘Hierarchy’ 탭 아래부분으로 이동하자. 그리고 상단 맥북 메뉴에서 Windows &gt; General &gt; Console을 눌러 콘솔창을 띄운 뒤 ‘Inspector’...","categories": [],
+        "tags": [],
+        "url": "/Unity-Programming(2)/",
+        "teaser": null
+      },{
+        "title": "First post",
+        "excerpt":"Hello   It’s my first post   -   Here will be my footprints and what I did before   Nice to meet you!  ","categories": [],
+        "tags": [],
+        "url": "/first-post/",
+        "teaser": null
+      },{
+        "title": "게임 개발 개요 정리글",
+        "excerpt":"1. 게임 기술 정리글     https://www.itfind.or.kr/WZIN/jugidong/1192/119206.htm  ","categories": [],
+        "tags": [],
+        "url": "/%EA%B2%8C%EC%9E%84-%EA%B0%9C%EB%B0%9C-%EA%B0%9C%EC%9A%94-%EC%A0%95%EB%A6%AC%EA%B8%80/",
+        "teaser": null
+      },{
+        "title": "Unitiy programming(3)",
+        "excerpt":"2. 상속과 컴포넌트 유니티는 기본적인 객체지향(OOP)의 특성인 상속과 게임 오브젝트 구성을 위해 상속보다 더 효과적인 컴포넌트 패턴이라는 것을 지원한다. 상속 일반적인 객체지향에서의 상속과 동일하다. 부모클래스의 메소드와 필드를 자식개체가 물려받아 활용할 수 있다. 다만 Human 클래스를 만들어 이를 바탕으로 다양한 NPC를 만들고 몬스터 오브젝트를 만들려고 하면 인간 종족이 아닌 슬라임, 늑대인간...","categories": [],
+        "tags": [],
+        "url": "/Unitiy-Programming(3)/",
+        "teaser": null
+      }]
